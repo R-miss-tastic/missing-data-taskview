c783f40c7a8eb4daeb7940b41dd4f42336ee42bc
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-10-18 14:30:40 +1100

    add my reviewed data

--- R/download-from-gdrive.R
+++ R/download-from-gdrive.R
@@ -2,15 +2,19 @@
 
 library(googledrive)
 
-googledrive::drive_download(file = "missing-data-taskview",
-                            path = "data/missing-data-taskview-njt.csv")
+# googledrive::drive_download(file = "missing-data-taskview",
+#                             path = "data/missing-data-taskview-njt.csv")
 
 
 library(tidyverse)
-miss_tv <- read_csv("missing-data-taskview-njt.csv")
+miss_tv <- read_csv(here::here("data", "missing-data-taskview.csv"))
 
 miss_tv %>% 
   filter(reviewer == "nick",
          decision == TRUE,
          updated_in_12m == TRUE,
-         vignette == TRUE)
+         vignette == TRUE) %>%
+  select(pkg_name,
+         comments,
+         everything()) %>%
+  View()
